ac555394eaaaeb8d54a0bf5713fd52ea6226e871
C4RC0 <karesz.carco@gmail.com>
2021-01-07 19:22:30 +0100

    initial commit

--- src/client/index.js
+++ src/client/index.js
@@ -39,7 +39,7 @@ export function run(p = defaultConfig) {
 
     if (typeof DEV !== "undefined" && DEV && module.hot){
         app.hot = module.hot;
-        module.hot.accept("./index");
+        module.hot.accept();
     }
 
     return wapp;
